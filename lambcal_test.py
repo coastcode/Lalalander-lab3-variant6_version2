6b16e58616f78d2e010f39940d0edaa776a85183
coastcode <81280412+coastcode@users.noreply.github.com>
2022-06-16 20:51:29 +0800

    Update and rename foo_test.py to lambcal_test.py

---
+++ lambcal_test.py
@@ -0,0 +1,38 @@
+import unittest
+from hypothesis import given,  strategies
+from lambcal import *
+
+# class LambcalTest(unittest.TestCase):
+#     def test_Lambcal(self):
+#         i = ''
+#         print('oral')
+#         print(x)
+#         for _ in range(10):
+#             x=x.beta()
+#             print(x)   
+class LexTest(TestCase):
+    def test_first_alpha_reduction(self):
+        result = lambda_cli_lex("((lambda x y. (x y)) y)")
+        self.assertEqual(next(result), LeftBracket)
+        self.assertEqual(next(result), LeftBracket)
+        self.assertEqual(next(result), LambdaKeyword)
+        var = next(result)
+        self.assertIsInstance(var, Parameter)
+        self.assertEqual(var.name, 'x')
+        var = next(result)
+        self.assertIsInstance(var, Parameter)
+        self.assertEqual(var.name, 'y')
+        self.assertEqual(next(result), Dot)
+        self.assertEqual(next(result), LeftBracket)
+        var = next(result)
+        self.assertIsInstance(var, Variable)
+        self.assertEqual(var.name, 'x')
+        var = next(result)
+        self.assertIsInstance(var, Variable)
+        self.assertEqual(var.name, 'y')
+        self.assertEqual(next(result), RightBracket)
+        self.assertEqual(next(result), RightBracket)
+        var = next(result)
+        self.assertIsInstance(var, Variable)
+        self.assertEqual(var.name, 'y')
+        self.assertEqual(next(result), RightBracket)
