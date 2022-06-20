c2cde6fbe9da8a41b0087af12c26c24c1e624beb
Huang Yuting <hyut@hdu.edu.cn>
2022-06-20 16:44:25 +0800

    Fixed partial code.

--- visu_factorial.py
+++ visu_factorial.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 
 class Lambda(object):
@@ -11,7 +11,7 @@ class Lambda(object):
         cur = self.node
         while cur.next is not None:
             cur = cur.next
-        cur.next = Any
+        cur.next = Optional[Any]
         cur.next = Node()
         cur = cur.next
         cur.label = "FACT"
