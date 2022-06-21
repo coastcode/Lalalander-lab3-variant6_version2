9e5edf2ecb324273a743c63390d310876e5250de
Huang Yuting <hyut@hdu.edu.cn>
2022-06-21 18:45:29 +0800

    Fixed type annotation.

--- lambcal.py
+++ lambcal.py
@@ -1,4 +1,4 @@
-from typing import Any, List, TypeVar
+from typing import Any, TypeVar
 from grammpy import Grammar, Nonterminal, Rule, EPS  # type: ignore
 import interpreter  # type: ignore
 # parsings
